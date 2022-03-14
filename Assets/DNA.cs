2b26c080d20c13f3b9a4ebe5546d4f4f2e8144ed
Mayuko Hayashi <mayukohayashi@MacBook-Air.local>
2022-03-14 13:35:21 +0900

    とりあえずここまで

--- Assets/DNA.cs
+++ Assets/DNA.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class DNA : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // gene for colour
+    public float r;
+    public float g;
+    public float b;
+
+    bool dead = false;
+    public float timeToDie = 0;
+
+    // 　使用できるようにする
+    SpriteRenderer sRenderer;
+    Collider2D sCollider;
+
+    void OnMouseDown()
     {
-        
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        Debug.Log("Dead at：" + timeToDie);　//clickしたかチェック
+
+        sRenderer.enabled = false;
+        sCollider.enabled = false;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+    }
+
     void Update()
     {
-        
+
     }
+
 }
---
+++ Assets/PopulationManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject personPrefab;
+    public int populationSize = 10;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
